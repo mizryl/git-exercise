5a62c07269992c9ba480e375bac27750f5996db7
mizryl <pham_linda@hotmail.com>
2025-11-19 17:49:18 -0800

    JUnit Testing And Inline code documentation added

--- SortInteger.java
+++ SortInteger.java
@@ -1,19 +1,26 @@
 public class SortInteger {
-    public static void main(String[] args) {
-        int[] num = {5,3,1,6,8,29};
-        int tempNum;
+    public static void sort(int[] num){
+        
+        
+        
+        //arrary with numbers to test the sorting
+        // int[] num = {5,3,1,6,8,29};
+        int tempNum; //temporay variable to store the number
 
+        //outer loop to ctrl how many passes we make
         for (int i = 0; i < num.length - 1; i++) {
+            //inner loop that deals w/ swapping
             for (int j = 0; j < num.length - 1 - i; j++) {
+                //if number is < the next num in the next index
                 if (num[j] > num[j+1]) {
-                    tempNum = num[j];
-                    num[j] = num[j + 1];
-                    num[j + 1] = tempNum;
+                    tempNum = num[j]; //store current value
+                    num[j] = num[j + 1]; //move small value to the left
+                    num[j + 1] = tempNum; //larger value to the right
                 } 
             }
         }
         for (int n : num) {
-            System.out.print(n + " ");
+            System.out.print(n + " "); //prints out sorted numbers
         }
     }
 }
